d1ef557f53d344a97d8f3b6296ba02b6e8a82bd5
BePoyerGabe <bernardopoyer00@gmail.com>
2022-03-19 19:03:27 -0300

    Autenticacao e token

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpResponse } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { UsuarioService } from './usuario/usuario.service';
+import { tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutenticacaoService {
 
-  constructor(private httpCliente: HttpClient) { }                      //Injeção de dependencia, a instancia é feita pelo Angular
+  constructor(private httpCliente: HttpClient, private usuarioService: UsuarioService) { }    //Injeção de dependencia, a instancia é feita pelo Angular
 
-  autentica(usuario: string, senha: string): Observable<any> {          //Promise "super", quando terminar retornara o objeto aqui definido na função
-    return this.httpCliente.post('http://localhost:3000/user/login', {
-      userName: usuario,
-      password: senha
-    })
+  autentica(usuario: string, senha: string): Observable<HttpResponse<any>> {                  //Promise "super", quando terminar retornara o objeto aqui definido na função
+    return this.httpCliente.post(
+      'http://localhost:3000/user/login',
+      {
+        userName: usuario,
+        password: senha
+      },
+      {
+        observe: 'response'      //não só o body, mas também o header
+      }
+    ).pipe(                     //Linka diferentes funções em uma só
+      tap((res) => {            // depois da requisição for concluida (Observable) executa qualquer outra função que seja necessária
+        const authToken = res.headers.get('x-access-token') ?? ''
+        this.usuarioService.salvaToken(authToken)
+      })
+    )
   }
+
 }
---
+++ src/app/autenticacao/usuario/usuario.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { TokenService } from '../token.service';
+import jwtDecode from 'jwt-decode';
+import { Usuario } from './usuario';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  /**
+   * Subject é um observable que permite não só pegar informações mas também enviar informações
+   * como um contexto em react
+   * 
+   * Behavior - todo componente/serviço que subscribe nesse subject ele envia o ultimo valor dele (guarda o estado)
+   */
+  private usuarioSubject = new BehaviorSubject<Usuario>({})
+
+  constructor(private tokenService: TokenService) {
+    if (tokenService.possuiToken()) {
+      this.decodificaJWT()
+    }
+  }
+
+  private decodificaJWT() {
+    const token = this.tokenService.retornaToken()
+    const usuario = jwtDecode(token) as Usuario
+
+    //atualiza o subject através da função next
+    this.usuarioSubject.next(usuario)
+  }
+
+  retornaUsuario() {
+    // envia como Observable pois somente esse serviço conseguirá modificar o subject
+    return this.usuarioSubject.asObservable()
+  }
+
+  salvaToken(token: string) {
+    this.tokenService.salvaToken(token)
+    this.decodificaJWT()
+  }
+
+  logout(token: string) {
+    this.tokenService.excluiToken(token)
+    this.usuarioSubject.next({})
+  }
+
+  isLogged() {
+    return this.tokenService.possuiToken()
+  }
+}
